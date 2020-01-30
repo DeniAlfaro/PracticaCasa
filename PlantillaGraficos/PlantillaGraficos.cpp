ff82f589c9bb0acceff4f15fa06ab3f6abad706d
alumno <alumno@E321>
2020-01-30 07:19:42 -0700

    yeayea

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -29,7 +29,7 @@ void dibujarPoligono() {
 	glEnd();
 }
 
-void dibujarTrianguloContinuo() {
+/*void dibujarTrianguloContinuo() {
 	glBegin(GL_TRIANGLE_STRIP);
 
 	glColor3f(1.0f, 1.0f, 1.0f);
@@ -42,22 +42,20 @@ void dibujarTrianguloContinuo() {
 	glVertex3f(0.2f, 0.15f, 0.0f);
 
 	glEnd();
-}
+}*/
+
 
 void dibujarLineaContinua() {
 	glBegin(GL_LINE_STRIP);
+	glColor3f(0.17f, 0.38f, 0.21f);
 
-	glColor3f(0.1f, 0.3f, 0.75f);
-
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.2f, 0.0f);
-	glVertex3f(0.4f, 0.2f, 0.0f);
-	glVertex3f(0.2f, 0.3f, 0.0f);
-
+	glVertex3f(-0.5f, -0.8f, 0.0f);
+	glVertex3f(-0.4f, -0.9f, 0.0f);
+	glVertex3f(-0.3f, -0.8f, 0.0f);
 	glEnd();
 }
 
-void dibujarLineas() {
+/*void dibujarLineas() {
 	glBegin(GL_LINES);
 	glColor3f(1.0f, 0.4f, 0.6f);
 
@@ -68,7 +66,7 @@ void dibujarLineas() {
 	glVertex3f(-0.3f, -0.4f, 0.0f);
 
 	glEnd();
-}
+}*/
 
 void dibujarTriangulos() {
 	//Establecemos el tipo de primitiva
@@ -81,9 +79,9 @@ void dibujarTriangulos() {
 			glVertex3f(-0.7f, -0.7f, 0.0f);
 			glColor3f(0.0f, 0.0f, 1.0f);
 			glVertex3f(0.7f, -0.7f, 0.0f);*/
-	//Especificar que dejaremos de dibujar
+			//Especificar que dejaremos de dibujar
 
-	//cesped
+			//cesped
 	glColor3f(0.26f, 0.68f, 0.13f);
 
 	glVertex3f(-1.0f, -0.6f, 0.0f);
@@ -154,10 +152,10 @@ void dibujarCirculo() {
 	//sol
 	glBegin(GL_POLYGON);
 	glColor3f(0.94f, 0.82f, 0.06f);
-	for (double i = 0; i < 360.0; i+=5.0) {
+	for (double i = 0; i < 360.0; i += 5.0) {
 		glVertex3f(
-			(0.15 * cos(i * 3.14159 / 180.0)) - 0.8, //tamaño X y offset X
-			(0.15 * sin(i * 3.14159 / 180.0)) + 0.8, //tamaño Y y offset Y
+			(0.15 * cos(i * 3.14159 / 180.0)) - 0.7, //tamaño X y offset X
+			(0.15 * sin(i * 3.14159 / 180.0)) + 0.7, //tamaño Y y offset Y
 			0.0f);
 	}
 	glEnd();
@@ -203,21 +201,126 @@ void dibujarCirculo() {
 	}
 	glEnd();
 
+	//perilla
 	glBegin(GL_POLYGON);
-	glColor3f(0.94f, 0.82f, 0.06f);
+	glColor3f(0.62f, 0.25f, 0.07f);
 	for (double i = 0; i < 360.0; i += 5.0) {
 		glVertex3f(
-			(0.05 * cos(i * 3.14159 / 180.0)) - 0.0, //tamaño X y offset X
-			(0.05 * sin(i * 3.14159 / 180.0)) + 0.0, //tamaño Y y offset Y
+			(0.05 * cos(i * 3.14159 / 180.0)) - (-0.55), //tamaño X y offset X
+			(0.05 * sin(i * 3.14159 / 180.0)) + (-0.5), //tamaño Y y offset Y
+			0.0f);
+	}
+	glEnd();
+
+	//arbolito
+	glBegin(GL_POLYGON);
+	glColor3f(0.26f, 0.68f, 0.13f);
+	for (double i = 0; i < 360.0; i += 5.0) {
+		glVertex3f(
+			(0.14 * cos(i * 3.14159 / 180.0)) - 0.75, //tamaño X y offset X
+			(0.17 * sin(i * 3.14159 / 180.0)) + (-0.2), //tamaño Y y offset Y
+			0.0f);
+	}
+	glEnd();
+	glBegin(GL_POLYGON);
+	glColor3f(0.26f, 0.68f, 0.13f);
+	for (double i = 0; i < 360.0; i += 5.0) {
+		glVertex3f(
+			(0.14 * cos(i * 3.14159 / 180.0)) - 0.60, //tamaño X y offset X
+			(0.17 * sin(i * 3.14159 / 180.0)) + (-0.079), //tamaño Y y offset Y
 			0.0f);
 	}
 	glEnd();
 }
 
+void dibujarLineas() {
+	glBegin(GL_LINES);
+	//ventana afuera
+	glColor3f(0.16f, 0.08f, 0.03f);
+
+
+	glVertex3f(0.13f, -0.13f, 0.0f);
+	glVertex3f(-0.13f, -0.13f, 0.0f);
+
+	glVertex3f(-0.13f, -0.13f, 0.0f);
+	glVertex3f(-0.13f, 0.13f, 0.0f);
+
+	glVertex3f(-0.13f, 0.13f, 0.0f);
+	glVertex3f(0.13f, 0.13f, 0.0f);
+
+	glVertex3f(0.13f, 0.13f, 0.0f);
+	glVertex3f(0.13f, -0.13f, 0.0f);
+
+
+	//ventana
+	glVertex3f(0.13f, 0.0f, 0.0f);
+	glVertex3f(-0.13f, 0.0f, 0.0f);
+
+	glVertex3f(0.0f, 0.13f, 0.0f);
+	glVertex3f(0.0f, -0.13f, 0.0f);
+
+
+	//solecito
+	glColor3f(0.94f, 0.82f, 0.06f);
+	glVertex3f(-0.5f, 0.8f, 0.0f);
+	glVertex3f(-0.4f, 0.85f, 0.0f);
+
+	glVertex3f(-0.5f, 0.9f, 0.0f);
+	glVertex3f(-0.55f, 0.85f, 0.0f);
+
+	glVertex3f(-0.48f, 0.7f, 0.0f);
+	glVertex3f(-0.4f, 0.7f, 0.0f);
+
+	glVertex3f(-0.5f, 0.6f, 0.0f);
+	glVertex3f(-0.4f, 0.55f, 0.0f);
+
+	glVertex3f(-0.55f, 0.55f, 0.0f);
+	glVertex3f(-0.48f, 0.45f, 0.0f);
+
+	glVertex3f(-0.6f, 0.48f, 0.0f);
+	glVertex3f(-0.55f, 0.4f, 0.0f);
+
+	glVertex3f(-0.7f, 0.48f, 0.0f);
+	glVertex3f(-0.7f, 0.37f, 0.0f);
+
+	glVertex3f(-0.8f, 0.5f, 0.0f);
+	glVertex3f(-0.85f, 0.42f, 0.0f);
+
+	glVertex3f(-0.88f, 0.55f, 0.0f);
+	glVertex3f(-0.95f, 0.45f, 0.0f);
+
+	glVertex3f(-0.9f, 0.6f, 0.0f);
+	glVertex3f(-0.95f, 0.58f, 0.0f);
+
+	glVertex3f(-1.0f, 0.7f, 0.0f);
+	glVertex3f(-0.92f, 0.7f, 0.0f);
+
+	glVertex3f(-0.9f, 0.78f, 0.0f);
+	glVertex3f(-0.98f, 0.82f, 0.0f);
+
+	glVertex3f(-0.88f, 0.82f, 0.0f);
+	glVertex3f(-0.94f, 0.88f, 0.0f);
+
+	glVertex3f(-0.82f, 0.88f, 0.0f);
+	glVertex3f(-0.9f, 0.94f, 0.0f);
+
+	glVertex3f(-0.76f, 0.90f, 0.0f);
+	glVertex3f(-0.82f, 0.96f, 0.0f);
+
+	glVertex3f(-0.7f, 0.92f, 0.0f);
+	glVertex3f(-0.7f, 0.98f, 0.0f);
+
+	glVertex3f(-0.62f, 0.90f, 0.0f);
+	glVertex3f(-0.58f, 0.98f, 0.0f);
+
+	glEnd();
+}
 
 void dibujar() {
 	dibujarTriangulos();
 	dibujarCirculo();
+	dibujarLineas();
+	dibujarLineaContinua();
 }
 
 int main()
